542997ee6aa87d6fea116602145c1dea9ade71c4
Samarth Singh Thakur <samarth.thakur44@gmail.com>
2020-08-19 22:36:22 +0530

    it's working

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -3,7 +3,8 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from . import forms
 
+
 class SignUp(CreateView):
     form_class = forms.UserCreationForm
-    success_url=reverse_lazy('login')
-    template_name='account/signup.html'
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
